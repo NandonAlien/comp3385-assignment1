02d56084215e8b3756323b537a67efd80f602f20
Nandon <nandonarts@gmail.com>
2024-02-12 13:29:20 -0400

    finishing the controller

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
  <h2>Norris Isaac</h2>
- <p>Student of the UWI FIC in their final year, who is hoping to do well this semester in order to pass. He likes creating things in general. </p>
+ <p>Student of the UWI FIC in their final year, who is hoping to do well this semester in order to pass.<br> He likes creating things in general and wonders about his future but he tends to accept that it isnt within his control.
+ </p>
 @endsection 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controller\AboutController; 
+use App\Http\Controllers\AboutController; 
 use Illuminate\Support\Facades\Route;
 
 
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', ['AboutController'::class, 'show']);
+Route::get('/about', [AboutController::class, 'show']);
