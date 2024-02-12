bb3749a896eacf2e94ce2cdb569979c0b06b2c00
Nandon <nandonarts@gmail.com>
2024-02-12 15:36:38 -0400

    correction of previous commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,4 +3,6 @@
  <h2>Norris Isaac</h2>
  <p>Student of the UWI FIC in their final year, who is hoping to do well this semester in order to pass.<br> He likes creating things in general and wonders about his future but he tends to accept that it isnt within his control.
  </p>
+
+ <img src="{{ url('/images/wt5l0xh5z7581.gif') }}" alt="Spinning Cryogen" /> 
 @endsection 
