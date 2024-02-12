a04d1ba80ad19fb36df641673da09cdd08a431a2
Nandon <nandonarts@gmail.com>
2024-02-12 13:24:34 -0400

    Current Progress of Controller making

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+ <h2>Norris Isaac</h2>
+ <p>Student of the UWI FIC in their final year, who is hoping to do well this semester in order to pass. He likes creating things in general. </p>
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controller\AboutController; 
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return "My About Page";
-});
+Route::get('/about', ['AboutController'::class, 'show']);
